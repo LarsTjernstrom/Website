c79f0384aeeaf41d03d2fe702145d1d6a0abf3ed
Anton Gerasimov <me@untone.net>
2017-05-11 19:20:18 +0300

    Added commit hooks for WebMap (not completed)

---
+++ src/WebsiteProvider/Api/CommitHooks.cs
@@ -0,0 +1,27 @@
+﻿using Simplified.Ring6;
+using Starcounter;
+
+namespace WebsiteProvider
+{
+    internal class CommitHooks
+    {
+        public void Register()
+        {
+            Hook<WebMap>.CommitInsert += (s, webMap) =>
+            {
+                string token = webMap.GetMappingToken();
+                Blender.MapUri(webMap.ForeignUrl, token);
+            };
+
+            Hook<WebMap>.BeforeDelete += (s, webMap) =>
+            {
+                string token = webMap.GetMappingToken();
+                Blender.UnmapUri(webMap.ForeignUrl, token);
+            };
+
+            //Hook<WebMap>.CommitUpdate += (s, webMap) =>
+            //{
+            //};
+        }
+    }
+}
