5d6a9a1ff60959f073c795612c0d5d57730b9bbd
warpech <warpech@gmail.com>
2017-06-23 02:49:13 +0200

    run the test apps

--- run.bat
+++ run.bat
@@ -2,5 +2,7 @@
 
 IF "%CONFIGURATION%"=="" SET CONFIGURATION=Debug
 
-star --resourcedir="%~dp0src\Website\wwwroot" "%~dp0src\Website\bin\%CONFIGURATION%\Website.exe"
-star --resourcedir="%~dp0src\WebsiteProvider\wwwroot" "%~dp0src\WebsiteProvider\bin\%CONFIGURATION%\WebsiteProvider.exe"
+REM star --resourcedir="%~dp0src\Website\wwwroot" "%~dp0src\Website\bin\%CONFIGURATION%\Website.exe"
+REM star --resourcedir="%~dp0src\WebsiteProvider\wwwroot" "%~dp0src\WebsiteProvider\bin\%CONFIGURATION%\WebsiteProvider.exe"
+star --resourcedir="%~dp0test\WebsiteProvider_AcceptanceHelperOne\wwwroot" "%~dp0test/WebsiteProvider_AcceptanceHelperOne/bin/%Configuration%/WebsiteProvider_AcceptanceHelperOne.exe"
+star --resourcedir="%~dp0test\WebsiteProvider_AcceptanceHelperTwo\wwwroot" "%~dp0test/WebsiteProvider_AcceptanceHelperTwo/bin/%Configuration%/WebsiteProvider_AcceptanceHelperTwo.exe"
